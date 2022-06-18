02fdd000b097a76b7f4035dbdcfa70f0ebfa6a31
Lightbulb <1286482110@qq.com>
2022-06-18 12:18:33 +0800

    Revert "Add retrieval from backend."
    This reverts commit daf7343696be90bad3093bc84936bf2bc4ebfc06.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -77,7 +77,6 @@ const DesktopNav = () => {
   const linkHoverColor = useColorModeValue('gray.800', 'white');
   const popoverContentBgColor = useColorModeValue('white', 'gray.800');
   const toast = useToast();
-  const currentURL = window.location.pathname;
   return (
     <Stack direction={'row'} spacing={4}>
       {NAV_ITEMS.map((navItem) => (
@@ -87,7 +86,7 @@ const DesktopNav = () => {
               {navItem.isImplemented ? (
                 <Link
                   p={2}
-                  href={navItem.href ? (navItem.href === currentURL ? '#' : navItem.href) : '#'}
+                  href={navItem.href ?? '#'}
                   fontSize={'sm'}
                   fontWeight={500}
                   color={linkColor}
@@ -320,11 +319,6 @@ const NAV_ITEMS: Array<NavItem> = [
   {
     label: '投稿提交',
     href: '/contribute',
-  },
-  {
-    label: '欣赏小作文',
-    href: '/view',
-    isImplemented: true,
   },
   {
     label: '其他',
--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -1,63 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { Textarea, Text, useToast, Box, Container, Link } from '@chakra-ui/react';
+import { Textarea } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import WithSubnavigation from '../components/header';
-import axios from 'axios';
 
 const ViewView: React.FC = () => {
-  const toast = useToast();
-  const [recommendations, setRecommendations] = useState<any[]>([]); 
-  useEffect(() => {
-    fetchRecommendations();
-  }, []);
-  const fetchRecommendations = async () => {
-    const response = await axios.get("/api/recommendations/0").catch((error) => {
-      console.log(error);
-      toast({
-        duration: 3000,
-        isClosable: false,
-        render: () => (
-          <Box color="white" rounded="lg" p={3} bg="#09958b">
-            Unable to fetch recommendations.
-          </Box>
-        ),
-      })
-    });
-    if (!response) return;
-    const data = response.data;
-    const entries = data.entries;
-    setRecommendations(entries);
-  }
   return (
     <div>
       <WithSubnavigation />
-      {
-        recommendations.map((item, id) => {
-          let url: string = item.source;
-          if (url !== "" && !url.startsWith("http")) {
-            url = "https://" + url
-          }
-          return (
-            <Box border='1px' rounded="lg" my='2' borderColor='gray.200' key={id}>
-              <Text fontSize='xl' borderBottom="1px">
-                {item.artist} - {item.title}
-              </Text>
-              <Text align="left" m='2'>
-                {item.description}
-              </Text>
-              <Text align="right" m='2' fontSize="sm">
-                {item.recommender}
-                <br></br>
-                {
-                  url !== ""
-                  ? <Link href={url}>{item.source}</Link>
-                  : ""
-                }
-              </Text>
-            </Box>
-          )
-        })
-      }
+      <Textarea />
     </div>
   );
 };
